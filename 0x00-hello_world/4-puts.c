d36d0dd25f144427d8316be093bd2b98f0c35100
Teme99 <3t49631@gmail.com>
2021-08-26 05:58:21 -0700

    still indenting q4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
+ *  * main - start excuting from here
  *   *
- *    * Return: Always 0 (Success)
+ *    * Return: 0 for success
  *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-	        return (0);
+		return (0);
 }
