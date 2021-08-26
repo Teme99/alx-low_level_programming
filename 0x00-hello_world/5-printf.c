e45bbbd35c67df9a26a0f0e17ee3fc1adf2dc529
Teme99 <3t49631@gmail.com>
2021-08-26 06:26:43 -0700

    updated the whitespace on q5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(void)
+/**
+* main - start excuting from here
+*
+* Return: 0 for success
+*/
+
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+		return (0);
 }
