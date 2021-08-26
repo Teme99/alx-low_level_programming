570abbc44990103bbff5b979dd4563cfd8fc47d2
Teme99 <3t49631@gmail.com>
2021-08-26 06:29:28 -0700

    took the comment above the main

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main(void)
 /**
 * main - start excuting from here
 *
 * Return: 0 for success
 */
+int main(void)
 
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
