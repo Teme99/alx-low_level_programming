a13c91ac285aa7530d82f53b79125659913fe68e
Teme99 <3t49631@gmail.com>
2021-08-26 06:02:36 -0700

    removed the extra 8s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - start excuting from here
- *   *
- *    * Return: 0 for success
- *     */
+ *   main - start excuting from here
+ *   
+ *     Return: 0 for success
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
