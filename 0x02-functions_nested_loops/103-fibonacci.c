61faeccdb241430b3167310e61516139b4e6b1cc
Teme99 <3t49631@gmail.com>
2021-08-31 11:36:08 -0700

    workied on q 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - where execution starts
+ *
+ * Description: fib numbers until the fib value itself
+ * 
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i = 1, j = 2, total = 0;
+	int k;
+
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			total += j;
+
+		k = j;
+		j += i;
+		i = k;
+	}
+	printf("%d\n", total);
+	return (0);
+}
