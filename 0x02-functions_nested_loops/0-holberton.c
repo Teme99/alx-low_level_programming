558743a21b69f85c9641a5201040941e6fd30e9e
Teme99 <3t49631@gmail.com>
2021-08-31 02:47:38 -0700

    solved q0, created debugging, created all its files and made them excutable

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - where execution starts
+ *
+ * Return: 0 for success
+ */
+
+int main() {
+
+   printf("Holberton \n");
+
+   return 0;
+}
+
