ee5025daf1013d34da5912e6014a097bfe5331c1
Teme99 <3t49631@gmail.com>
2021-09-02 05:11:03 -0700

    worked on q0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+fndef MAIN_H
+#define MAIN_H
+
+
+
+void positive_or_negative(int i);
+
+/**
+ * int big_num(int a, int b, int c);
+ * int convert_day(int month, int day);
+ * void print_remaining_days(int month, int day, int year);
+ */
+#endif /* MAIN_H */
