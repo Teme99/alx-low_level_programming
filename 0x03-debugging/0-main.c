92e86c25571964942de28d3f4a0ae9f89df49063
Teme99 <3t49631@gmail.com>
2021-09-02 09:08:44 -0700

    still working on q0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * main - where execution starts
- *
- * Return: 0 for success
- *
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
