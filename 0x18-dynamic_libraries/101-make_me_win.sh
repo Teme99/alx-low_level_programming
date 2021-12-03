bf2ce47d92234153aa9bdc4846c8a459023e0ea4
Teme99 <3t49631@gmail.com>
2021-12-03 04:29:04 -0800

    re woked on 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Teme99/alx-low_level_programming/main/0x18-dynamic_libraries/libinjection.so LD_PRELOAD="$PWD/../libinjection.so"
+gcc *.o -shared -o libruin.so
+LD_PRELOAD=$PWD/libruin.so
